6f33b5cc23699502b30738cd20a221ac5b5a857d
MyoMyintAung <myomyintaung064@gmail.com>
2022-03-13 11:13:40 +0630

    use useEffect clean up function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,10 @@ import MainHeader from "./components/MainHeader/MainHeader";
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // empty dependancy က စစချင်းတစ်ခါပဲ run တယ်၊ နောက်ထပ် page reloade/ refresh ဖြစ်သွားမှ ထပ် run တယ်
+  // refresh ဖြစ်သွားတိုင်း user loggin ဝင်ထားလားဆိုတာ localStroage ထဲဝင်စစ်တယ် ဝင်ထားတယ်ဆိုရင် manual loggin ဝင်ခဲ့သလိုမျိုး loggin ဝင်နေတဲ့ state ကို ရောက်အောင် ပို့ပေးတယ်
   useEffect(() => {
+    console.log("Runned useEffect");
     const userLoggedInInfo = localStorage.getItem("isLoggedIn");
     if (userLoggedInInfo === "1") {
       setIsLoggedIn(true);
--- my-app/src/components/Login/Login.js
+++ my-app/src/components/Login/Login.js
@@ -1,34 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  // settimeout တွေက key down တစ်ခုချင်းစီအတွက် stack ဖြစ်သွားတယ်။ အဲ့တော့ အများကြီး ပြုံစစ်သလိုဖြစ်နေတယ်
+
+  // ဘာလို့ နောက်ဆုံး setTimeout ပဲ ကျန်တာလဲဆိုတော့ useEffect ကို execute လုပ်တဲ့ order ကြောင့်
+  // အရင်ဆုံး load လုပ်တဲ့အခါ react က ပထမဆုံးအကြိမ် useEffect ထဲက function ကို run လိုက်တယ်
+  // useEffect ရဲ့ depencies ပြောင်းသွားလို့မဟုတ်ပဲ react က run လိုက်တာဖြစ်လို့ return မပြန်စေဘူး
+  // depencies ချိန်းမှ useEffect ကြီး တစ်ခုလုံးကို run စေတာကြောင့် အောက်က clean up function ကို run စေတယ်
+  // အဲ့တော့ user key stroke တချက်လုပ်လိုက်ရင် react run ထားခဲ့တဲ့ setTimeout ကို clear လုပ်တယ်
+  // ခုချက်ချင်း user ရိုက်ခဲ့တဲ့ setTimeout ကကျန်နေခဲ့တယ်။ ဒီနည်းနဲ့ နောက်ဆုံး user ရိုက်ပြီးတော့ time out မှ side effect ဖြစ်စေတဲ့ code ကို run စေတယ်
+
+  // component mount ဖြစ်ရင် useEffect function ကို ပထမဆုံး run တယ်
+  // depencies ချိန်းလို့ နောက်တခါထပ် run ဖြစ်ရင် clean up function return ကို အရင် run တယ်
+  useEffect(() => {
+    const idendifier = setTimeout(() => {
+      console.log("Checking Form Validaty.");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    // This is CLEAN UP function
+    //
+    return () => {
+      clearTimeout(idendifier);
+      console.log("clean up");
+    };
+  }, [enteredEmail, enteredPassword]);
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    setEmailIsValid(enteredEmail.includes("@"));
   };
 
   const validatePasswordHandler = () => {
@@ -45,7 +64,7 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
@@ -59,7 +78,7 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
